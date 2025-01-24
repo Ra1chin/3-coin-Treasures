00d0ebcd2e99237e03fbbdc8c0780c8ad4734494
Ra1chin <raichindesuma@gmail.com>
2025-01-24 21:15:01 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Function to show the popup
+function showPopup(id) {
+    document.getElementById(id).style.display = 'flex';
+}
+
+// Function to close the popup
+function closePopup(id) {
+    document.getElementById(id).style.display = 'none';
+}
